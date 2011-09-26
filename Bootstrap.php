29875b5ed4b79a6b2c983cf23999f234ec445aeb
Aaron Parecki <aaron@parecki.com>
2011-09-25 23:38:48 -0400

    Add google analytics to the header, optionally add another CSS file if defined in the config file. Minor css updates.

--- Bootstrap.php
+++ Bootstrap.php
@@ -50,7 +50,7 @@ class BootstrapTemplate extends QuickTemplate {
    * @access private
    */
   function execute() {
-		global $wgUser, $wgSitename, $wgCopyrightLink, $wgCopyright, $wgBootstrap, $wgArticlePath;
+		global $wgUser, $wgSitename, $wgCopyrightLink, $wgCopyright, $wgBootstrap, $wgArticlePath, $wgGoogleAnalyticsID, $wgSiteCSS;
 
 		$this->skin = $this->data['skin'];
 
@@ -67,7 +67,30 @@ class BootstrapTemplate extends QuickTemplate {
   	<script type="text/javascript" src="<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/js/bootstrap-tabs.js"></script>
   	<script type="text/javascript" src="<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/js/bootstrap-dropdown.js"></script>
   	<script type="text/javascript" src="<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/site.js"></script>
+    <?php if(isset($wgSiteCSS)) { ?>
+    	<link rel="stylesheet" type="text/css" href="<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/<?php echo $wgSiteCSS ?>" />
+    <?php } ?>
     <?php $this->html('headlinks') ?>
+    <?php 
+    
+    if(isset($wgGoogleAnalyticsID)) { ?>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '<?php echo $wgGoogleAnalyticsID; ?>']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+      <?php
+    }
+    ?>
 </head>
 <body class="<?=Sanitizer::escapeClass('page-' . $this->data['title'])?>">
 
